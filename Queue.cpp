ddc22d6e31c47687388cf4c85bdac8163bc44b6c
Justin Hoang <justin.lhoang3@gmail.com>
2024-03-15 18:46:12 -0600

    fix Queue

--- Queue.cpp
+++ Queue.cpp
@@ -1,121 +1,258 @@
 #include "Queue.h"
-#include "PCB.h"
-#include <cstring>
+#include <cassert>
+#include <cstdlib>
 
-int HEAD = 0;
-int TAIL = 1;
-int INCREASE = 1;
-int DECREASE = -1;
+template <typename T>
+Queue<T>::Queue(const Queue& d) {
+    arr = new T[d.cap];
+    cap = d.cap;
+    used = d.used;
 
-Queue::Queue() {
-    queue = new PCB[0];
-    size = 0;
+    for (int i = 0; i < used; ++i) {
+        arr[i] = d.arr[i];
+    }
 }
 
-Queue::~Queue() {
-    delete[] queue;
-}
-
-// sort
-void Queue::sort() {
-    PCB temp;
-    bool swapped = true;
-    int index = 0;
+template <typename T>
+const T& Queue<T>::operator [ ] (unsigned target) const {
+    try {
+        if (target>=used || target<0) {
+            throw badBracket(target);
+        }
+    }
+    catch(badBracket& b) {
+        // error message output by badBracket constructor
+    }
     
-    while (swapped) {
-        swapped = false;
-        index++;
+    return arr[target];
+}
 
-        for (int i = 0; i < size - index; i++) {
-            if (queue[i].getCPUBurst() > queue[i+1].getCPUBurst()) {
-                temp = queue[i];
-                queue[i] = queue[i+1];
-                queue[i+1] = temp;
-                swapped = true;
+template <typename T>
+T& Queue<T>::operator [ ] (unsigned target) {
+    try {
+        if (target>=used || target<0) {
+            throw badBracket(target);
+        } 
+    }
+    catch(badBracket& b) {
+        // error message output by badBracket constructor
+        // if (b.target > 0) { this.resize(b.target); } <--?
+    }
+    
+    return arr[target];
+}
+
+
+template <typename T>
+const T* Queue<T>::find(const T& target) const {
+    for (int i = 0; i < used; ++i) {
+        if (arr[i] == target) {
+            return arr[i];
+        }
+    }
+    
+    return 0;
+}
+
+template <typename T>
+T Queue<T>::front() const {
+    return arr[0];
+}
+
+template <typename T>
+T Queue<T>::back() const {
+    return arr[used-1];
+}
+
+template <typename T>
+T& Queue<T>::front() {
+    return arr[0];
+}
+
+template <typename T>
+T& Queue<T>::back() {
+    return arr[used-1];
+}
+
+template <typename T>
+T* Queue<T>::find(const T& target) {
+    for (int i = 0; i < used; ++i) {
+        if (arr[i] == target) {
+            return arr[i];
+        }
+    }
+    
+    return 0;
+} 
+
+template <typename T>
+void Queue<T>::pop_back() {
+    if (used > 0) {
+        T* temp = new T[cap];
+
+        for (int i = 0; i < used-1; ++i) {
+            temp[i] = arr[i];
+        }
+        delete [] arr;
+        arr = temp;
+        --used;
+    }
+}
+
+template <typename T>
+void Queue<T>::push_back(const T& addend) {
+    assert(used <= cap);
+
+    // resize array by 1 when needed
+    if (used + 1 == cap) {
+        T *newT = new T[cap+1]; 
+
+        for (int i = 0; i < used; ++i) {
+            newT[i] = arr[i];
+        }
+
+        newT[used] = addend;
+        ++cap;
+        ++used;
+        delete [] arr;
+        arr = newT;
+    } else {
+        arr[used] = addend;
+        ++used;
+    }
+}
+
+
+template <typename T>
+void Queue<T>::resize (unsigned newSize) {
+    T *newT = new T[newSize];
+    
+    for (int i = 0; i < newSize; ++i) {
+        if (i < newSize) {
+            newT[i] = arr[i];
+        }
+    }
+    delete [] arr;
+    arr = newT;
+
+    if (used > newSize) {
+        used = newSize;
+    }
+    cap = newSize;
+}
+
+
+template <typename T>
+Queue<T>& Queue<T>::operator = (const Queue<T>& rHS) {
+    
+    T* temp = new T[rHS.capacity()];
+    for (int i = 0; i < cap; ++i) {
+        temp[i] = rHS[i];
+    }
+    delete [] arr;
+    arr = temp;
+    used = rHS.size();
+    cap = rHS.capacity();
+    
+    return temp;
+}
+
+
+template <typename T>
+void Queue<T>::operator += (const Queue<T>& rHS) {
+    unsigned newSize = rHS.size() + used, pos = rHS.size();
+    T* newT = new T[newSize];
+
+    for (int i = 0; i < used; ++i) {
+        newT[i] = arr[i];
+    }
+    
+    for (int i = pos; i < newSize; ++i) {
+        newT[i] = rHS[i];
+    }
+
+    delete [] arr;
+    arr = newT;
+    used = newSize;
+    cap = newSize;
+}
+
+
+template <typename T>
+void Queue<T>::pop_front() {
+    if (used > 0) {
+        T* temp = new T[cap];
+
+        for (int i = 1; i < used; ++i) {
+            temp[i-1] = arr[i];
+        }
+        delete [] arr;
+        arr = temp;
+        --used;
+    }
+}
+
+
+template <typename T>
+void Queue<T>::push_front(const T& insert) {
+    if (used + 1 == cap) {
+        T *newT = new T[cap+1];
+
+        for (int i = 0; i < used; ++i) {
+            newT[i] = arr[i];
+        }
+        ++cap;
+        delete [] arr;
+        arr = newT;
+    }
+    
+    {
+        for (int i = used; i > 0; --i) {
+            arr[i] = arr[i-1];
+        }
+    }
+    
+    {
+        arr[0] = insert;
+        ++used;
+    }
+}
+
+template <typename T>
+void Queue<T>::moveToBack(int index) {
+    if (used > 0) {
+        T* temp = new T[cap];
+
+        for (int i = 0; i < used; ++i) {
+            if (i != index && i <= index) {
+                temp[i] = arr[i];
+            } else if (i > index) {
+                temp[i-1] = arr[i];
             }
         }
+
+        temp[used-1] = arr[index];
+        delete [] arr;
+        arr = temp;
     }
 }
 
-// push into queue
-void Queue::push(PCB& p) {
-    queue = resize_arr(INCREASE, TAIL);
-    queue[size-1] = p;
-}
 
-// pop from queue
-PCB& Queue::pop(PCB* p, int num_processes) {
-    PCB pcb = queue[0];
-    
-    for (int i = 0; i < num_processes; i++) {
-        if (queue[0].getPID() == p[i].getPID()) {
-            p[i].setCPUBurst(queue[0].getCPUBurst());
-            p[i].finished = queue[0].finished;
-            p[i].context_switch_num = queue[0].context_switch_num;
-            p[i].response_time = queue[0].response_time;
-            p[i].completion_time = queue[0].completion_time;
-            p[i].turnaround_time = queue[0].turnaround_time;
-            p[i].waiting = queue[0].waiting;
-            p[i].waiting_time = queue[0].waiting_time;
-            break;
+template <typename T>
+void Queue<T>::moveToFront(int index) {
+    if (used > 0) {
+        T* temp = new T[cap];
+
+        for (int i = 0; i < used; ++i) {
+            if (i != index && i <= index) {
+                temp[i+1] = arr[i];
+            } else if (i > index) {
+                temp[i] = arr[i];
+            }
         }
+        
+        temp[0] = arr[index];
+        delete [] arr;
+        arr = temp;
     }
-
-    queue = resize_arr(DECREASE, HEAD);
-
-    return pcb;
-}
-
-// remove from queue
-PCB& Queue::remove(int pos, PCB* p, int num_processes) {
-    PCB pcb = queue[pos];
-
-    for (int i = 0; i < num_processes; i++) {
-        if (queue[pos].getPID() == p[i].getPID()) {
-            p[i].setCPUBurst(queue[pos].getCPUBurst());
-			p[i].finished = queue[pos].finished;
-			p[i].context_switch_num = queue[pos].context_switch_num;
-			p[i].response_time = queue[pos].response_time;
-			p[i].completion_time = queue[pos].completion_time;
-			p[i].turnaround_time = queue[pos].turnaround_time;
-			p[i].waiting = queue[pos].waiting;
-			p[i].waiting_time = queue[pos].waiting_time;
-			break;
-        }
-    }
-}
-
-PCB* Queue::resize_arr(int mode, int from) {
-    size += mode;
-    PCB* new_queue = new PCB[size];
-
-    for (int i = 0; i < size; i++) {
-        if ((mode == INCREASE && from == TAIL) || (mode == DECREASE && from == TAIL)) {
-            new_queue[i] = queue[i];
-        } else {
-            new_queue[i] = queue[i+1];
-        }
-    }
-
-    return new_queue;
-}
-
-// gets items in queue
-PCB* Queue::get(int i) {
-    return &queue[i];
-}
-
-// length of queue (num of elements)
-int Queue::length() {
-    return size;
-}
-
-bool Queue::empty() {
-    bool empty = false;
-    
-    if (size == 0) {
-        empty = true;
-    }
-
-    return empty;
 }
--- Queue.h
+++ Queue.h
@@ -1,25 +1,57 @@
-#ifndef QUEUE_H
-#define QUEUE_h
+#ifndef _Queue
+#define _Queue
 
-#include "PCB.h"
+#include <iostream>
+#include <exception>
 
+template <typename T>
 class Queue {
-    private:
-        PCB* resize_arr(int, int);
-        PCB* queue;
-        int size;
+public:
+    Queue<T>() { arr = new T[1]; used = 0; cap = 1; }
+    Queue<T>(const Queue& d);
+    explicit Queue<T>(unsigned newCap)
+        { arr = new T[newCap]; used = 0; cap = newCap; }
+    ~Queue<T>() { delete [] arr; }
+    
+    // Constant members
+    unsigned size() const { return used; }
+    unsigned capacity() const { return cap; }
+    const T* find(const T&) const;
+    T front() const;
+    T back() const;
+    
+    // Modification members
+    T& back();
+    void pop_back();
+    void push_back(const T&);
+    T& front();
+    void pop_front();
+    void push_front(const T&);  // added 2015/07/08 for queue FIFO ops
+    T* find(const T&);
+    void resize(unsigned);
+    void moveToBack(int);
+    void moveToFront(int);
+    
+    // Operators
+    const T& operator [ ] (unsigned) const;
+    T& operator [ ] (unsigned);
+    Queue& operator = (const Queue&);
+    void operator += (const Queue&);
+    
+private:
+    T* arr;
+    unsigned used, cap;
 
+    class badBracket
+    {
     public:
-        Queue();
-        ~Queue();
+        badBracket(unsigned index)
+        {
+            std::cout << "\n**out of bounds, target = " << index << "**" << std::endl;
+        }
+    };
 
-        int length();
-        bool empty();
-        void sort();
-        void push(PCB&);
-        PCB& pop(PCB*, int);
-        PCB& remove(int, PCB*, int);
-        PCB* get(int);
 };
 
+#include "Queue.cpp"
 #endif
